c2cf43dd2b12e1353c83d683ebfcba444b86756b
MiniOcean404 <1037306928@qq.com>
2024-01-23 01:31:43 +0800

    chore: workflow

--- script/ci-rename-file.sh
+++ script/ci-rename-file.sh
@@ -6,18 +6,21 @@ for filename in prod/package/*/*.*
 do
   # 删除 prod/package/ 及其之前的部分
   BINDING_NAME=${filename#*prod/package/}
+  # 删除第一个斜杠及其之后的部分
   BINDING_ABI=${BINDING_NAME%%/*}
-  CLI_BINARY_PATH=${filename%%.*}
+  BINARY_PATH=${filename%%.*}
+
+  echo "文件名 $filename"
 
   echo "准备构建 图片压缩:"
   echo "绑定名 $BINDING_NAME"
   echo "文件名 $filename"
-  echo "二进制文件路径 $CLI_BINARY_PATH"
+  echo "二进制文件路径 $BINARY_PATH"
 
-  if [ -f "$CLI_BINARY_PATH" ]; then
-      chmod +x $CLI_BINARY_PATH
-      mv -v $CLI_BINARY_PATH ./prod_cli/swc-$BINDING_ABI
-  elif [ -f "$CLI_BINARY_PATH.exe" ]; then
-      mv -v $CLI_BINARY_PATH.exe ./prod_cli/swc-$BINDING_ABI.exe
+  if [ -f "$BINARY_PATH" ]; then
+      chmod +x $BINARY_PATH
+      mv -v $BINARY_PATH ./prod_cli/swc-$BINDING_ABI
+  elif [ -f "$BINARY_PATH.exe" ]; then
+      mv -v $BINARY_PATH.exe ./prod_cli/swc-$BINDING_ABI.exe
   fi
 done

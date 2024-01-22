0363433ac1545ceae28e78ea141d74d0943eb47e
MiniOcean404 <1037306928@qq.com>
2024-01-23 01:54:40 +0800

    chore: workflow

--- script/ci-rename-file.sh
+++ script/ci-rename-file.sh
@@ -2,17 +2,19 @@
 
 mkdir -p ./prod_cli
 # Naive substitution to napi artifacts for the cli binary.
-for filename in prod/package/*/*
+
+for path in prod/package/*/*
 do
   # 删除 prod/package/ 及其之前的部分
-  BINDING_NAME=${filename#*prod/package/}
-  # 删除第一个斜杠及其之后的部分
+  BINDING_NAME=${path#*/*/}
+  # 删除 BINDING_NAME 匹配的第一个斜杠及其之后的部分
   BINDING_ABI=${BINDING_NAME%%/*}
-  BINARY_PATH=${filename%%.*}
+  # 脚本原本是处理 xx.node 文件所有才会有 BINARY_PATH 这个变量扩展语法
+  BINARY_PATH=${path%%.*}
 
 
   echo "-------------------开始准备变量-------------------"
-  echo "文件名 $filename"
+  echo "文件路径 $path"
   echo "绑定名 $BINDING_NAME"
   echo "ABI 名称 $BINDING_NAME"
   echo "二进制文件路径 $BINARY_PATH"

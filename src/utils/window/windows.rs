6947468e01741d1432bb6e1835706bb77c3be285
MiniOcean404 <1037306928@qq.com>
2024-02-03 22:26:20 +0800

    feat: windows 库

--- src/utils/window/windows.rs
+++ src/utils/window/windows.rs
@@ -1,45 +1,40 @@
 #[cfg(windows)]
-extern crate winapi;
-
-use libc::c_void;
-use std::ffi::OsString;
-use std::os::windows::ffi::OsStringExt;
 use std::ptr::{self};
-use winapi::shared::minwindef::{DWORD, FALSE, LPARAM, UINT};
-use winapi::shared::windef::HWND;
-use winapi::um::combaseapi::CoCreateInstance;
-use winapi::um::processthreadsapi::OpenProcess;
-use winapi::um::shtypes::LPITEMIDLIST;
-use winapi::um::winbase::QueryFullProcessImageNameW;
-use winapi::um::winuser::{GetForegroundWindow, GetWindowThreadProcessId};
 
-#[cfg(windows)]
-pub fn get_windows_program_path() {
-    unsafe {
-        let hwnd = GetForegroundWindow();
-        let mut pid: DWORD = 0;
-        GetWindowThreadProcessId(hwnd, &mut pid);
+use windows::Win32::{System::Com::CoCreateInstance, UI::Shell::CSIDL_WINDOWS};
 
-        let process_handle = OpenProcess(winapi::um::winnt::PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
-
-        if !process_handle.is_null() {
-            let mut buffer: Vec<u16> = vec![0; 4096];
-            let mut size: DWORD = buffer.len() as DWORD;
-            if QueryFullProcessImageNameW(process_handle, 0, buffer.as_mut_ptr(), &mut size) != 0 {
-                let os_str = OsString::from_wide(&buffer[..(size as usize)]);
-                let process_path = os_str.to_string_lossy();
-                println!("应用程序路径: {}", process_path);
-            }
-        }
-    }
+pub fn test() {
+    // unsafe { CoCreateInstance(&CSIDL_WINDOWS, ptr::null_mut(), 1) };
+    // println!("Current folder path: {:?}", path);
 }
+
+// #[cfg(windows)]
+// pub fn get_windows_program_path() {
+//     unsafe {
+//         let hwnd = GetForegroundWindow();
+//         let mut pid: DWORD = 0;
+//         GetWindowThreadProcessId(hwnd, &mut pid);
+
+//         let process_handle = OpenProcess(winapi::um::winnt::PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
+
+//         if !process_handle.is_null() {
+//             let mut buffer: Vec<u16> = vec![0; 4096];
+//             let mut size: DWORD = buffer.len() as DWORD;
+//             if QueryFullProcessImageNameW(process_handle, 0, buffer.as_mut_ptr(), &mut size) != 0 {
+//                 let os_str = OsString::from_wide(&buffer[..(size as usize)]);
+//                 let process_path = os_str.to_string_lossy();
+//                 println!("应用程序路径: {}", process_path);
+//             }
+//         }
+//     }
+// }
 
 // Define the ExplorerFolderInfo struct
-#[repr(C)]
-pub struct ExplorerFolderInfo {
-    hwnd: HWND,
-    pidl: LPITEMIDLIST,
-}
+// #[repr(C)]
+// pub struct ExplorerFolderInfo {
+//     hwnd: HWND,
+//     pidl: LPITEMIDLIST,
+// }
 
 // https://pianshen.com/question/68891958968/
 // fn get_current_explorer_folders() -> Result<Vec<ExplorerFolderInfo>, String> {

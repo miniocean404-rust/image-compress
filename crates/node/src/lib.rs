0cd42f5b61df934fa044fdea7928a3a603b97b7e
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-02-04 18:01:44 +0800

    feat: 压缩

--- crates/node/src/lib.rs
+++ crates/node/src/lib.rs
@@ -1,11 +1,19 @@
+use image_compress_core::compress::index::ImageCompression;
 /// import the preludes
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
 
-#[napi]
-fn test(n: u32) -> u32 {
-    match n {
-        1 | 2 => 1,
-        _ => test(n - 1) + test(n - 2),
-    }
+// #[napi(js_name = "Compiler")]
+// pub struct impl ImageCompression {}
+
+#[napi::module_init]
+fn init() {}
+
+#[napi(js_name = "compress_test")]
+fn compress(file: String) -> Vec<u8> {
+    let mut info = ImageCompression::new(file, 80).unwrap();
+    // AsyncTask::with_optional_signal(info.start_mem_compress(false), None);
+    info.mem
 }
+
+// #[napi(ts_return_type = "Promise<{ [index: string]: { code: string, map?: string } }>")]
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+const a = require("./dist/node/miniocean_image_compress.darwin-x64.node");
 
+console.log(a.test(15));

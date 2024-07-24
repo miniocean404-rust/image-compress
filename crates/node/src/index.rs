9365e124687a0b5d07ac44c7a23f6a004bbd1fec
MiniOcean404 <1037306928@qq.com>
2024-07-24 21:07:06 +0800

    feature: wasm node 打包

--- crates/node/src/index.rs
+++ crates/node/src/index.rs
@@ -1,16 +1,15 @@
-use napi_derive::napi;
-
-use backtrace::Backtrace;
-
 use std::env;
 use std::panic::set_hook;
+
+use backtrace::Backtrace;
+use napi_derive::napi;
 
 #[napi::module_init]
 fn init() {
     if cfg!(debug_assertions) || env::var("CUSTOM_DEBUG").unwrap_or_default() == "1" {
         set_hook(Box::new(|panic_info| {
             let backtrace = Backtrace::new();
-            println!("Panic: {:?}\nBacktrace: {:?}", panic_info, backtrace);
+            println!("恐慌: {:?}\n回溯: {:?}", panic_info, backtrace);
         }));
     }
 }

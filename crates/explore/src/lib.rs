53a2e1d70044baa76ad85fbe3a601e22ec136490
MiniOcean404 <1037306928@qq.com>
2024-07-26 00:28:34 +0800

    feature: 暴露方法

--- crates/explore/src/lib.rs
+++ crates/explore/src/lib.rs
@@ -1,2 +1,19 @@
-pub mod macos;
-pub mod windows;
+#[cfg(target_os = "macos")]
+use crate::macos::index::get_finder_info;
+#[cfg(target_os = "windows")]
+use crate::windows::index::get_explore_info;
+
+mod macos;
+mod windows;
+
+pub unsafe fn get_os_dir() -> anyhow::Result<()> {
+    #[cfg(target_os = "macos")]
+    let info = unsafe { get_finder_info()? };
+
+    #[cfg(target_os = "windows")]
+    let info = unsafe { get_explore_info()? };
+
+    dbg!(info);
+
+    anyhow::Ok(())
+}
--- crates/explore/src/main.rs
+++ crates/explore/src/main.rs
@@ -1,14 +1,5 @@
-#[cfg(target_os = "macos")]
-use explore::macos::index::get_finder_info;
-#[cfg(target_os = "windows")]
-use explore::windows::index::get_explore_info;
+use explore::get_os_dir;
 
 fn main() {
-    #[cfg(target_os = "macos")]
-    let info = unsafe { get_finder_info().unwrap() };
-
-    #[cfg(target_os = "windows")]
-    let info = unsafe { get_explore_info().unwrap() };
-
-    println!("{:?}", info)
+    unsafe { println!("{:?}", get_os_dir().unwrap()) }
 }

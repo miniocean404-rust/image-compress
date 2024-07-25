8c690cc1d8526cfaec7ff710952f5dd3cbc99cc6
MiniOcean404 <1037306928@qq.com>
2024-07-26 01:12:30 +0800

    feature: 环境判断

---
+++ crates/utils/src/env.rs
@@ -0,0 +1,19 @@
+pub fn is_debug() -> bool {
+    cfg!(debug_assertions)
+}
+
+pub fn is_release() -> bool {
+    !cfg!(debug_assertions)
+}
+
+pub fn is_windows() -> bool {
+    cfg!(target_os = "windows")
+}
+
+pub fn is_macos() -> bool {
+    cfg!(target_os = "macos")
+}
+
+pub fn is_linux() -> bool {
+    cfg!(target_os = "linux")
+}
--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod env;
 pub mod file;
 pub mod log;
 pub mod sleep;

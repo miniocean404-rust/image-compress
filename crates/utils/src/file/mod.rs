2d740561638484a4992377053062af3f64b13afa
MiniOcean404 <1037306928@qq.com>
2024-07-27 01:13:11 +0800

    feature: 整理

--- crates/utils/src/file/mod.rs
+++ crates/utils/src/file/mod.rs
@@ -1,2 +1,1 @@
-pub mod deep;
 pub mod mime;
--- crates/utils/src/path/deep.rs
+++ crates/utils/src/path/deep.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::path::PathBuf;
+
 use globset::{Glob, GlobSetBuilder};
 use walkdir::{DirEntry, WalkDir};
 
@@ -36,4 +39,30 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .to_str()
         .map(|s| s.starts_with("."))
         .unwrap_or(false)
+}
+
+// 递归文件夹
+pub async fn read_dir_path_buf(path: &str) -> anyhow::Result<Vec<PathBuf>> {
+    fn deep_dir(p: &str) -> anyhow::Result<Vec<PathBuf>> {
+        let entries = fs::read_dir(p)?;
+
+        let vec_path_buf = entries
+            .filter_map(|entry| entry.ok())
+            .flat_map(|entry| {
+                let meta = entry.metadata().ok()?;
+                if meta.is_file() {
+                    Some(vec![entry.path()])
+                } else if meta.is_dir() {
+                    deep_dir(entry.path().to_str().unwrap()).ok()
+                } else {
+                    None
+                }
+            })
+            .flatten()
+            .collect();
+
+        Ok(vec_path_buf)
+    }
+
+    deep_dir(path)
 }

e7099a18c1f2af415bb0222abbaa600bd3663181
MiniOcean404 <1037306928@qq.com>
2024-07-28 16:41:55 +0800

    👷 construction_worker: 测试

--- .github/scripts/cli_artifacts_rename.sh
+++ .github/scripts/cli_artifacts_rename.sh
@@ -3,7 +3,7 @@
 cd ./packages/
 
 # 将归档的文件重命名移动到另一个文件夹中
-ENTRY_DIR="./artifacts/*/*.node"
+ENTRY_DIR="artifacts/*/*.node"
 OUTPUT_DIR="./artifacts_rename"
 PROJECT_BINDING_NAME="image-compress"
 
@@ -12,7 +12,9 @@ mkdir -p $OUTPUT_DIR
 for filename in $ENTRY_DIR
 do
   BINDING_NAME=${filename#*.}
+  # 获取
   BINDING_ABI=${BINDING_NAME%%.*}
+  # 获取文件后缀名
   CLI_BINARY_PATH=${filename%%.*}
 
   echo "-------------------开始准备变量-------------------"
@@ -24,13 +26,13 @@ do
 
   if [ -f "$CLI_BINARY_PATH" ]; then
       chmod +x $CLI_BINARY_PATH
-      mv -v $CLI_BINARY_PATH ./$OUTPUT_DIR/$PROJECT_BINDING_NAME-$BINDING_ABI
+      mv -v $CLI_BINARY_PATH $OUTPUT_DIR/$PROJECT_BINDING_NAME-$BINDING_ABI
   elif [ -f "$CLI_BINARY_PATH.exe" ]; then
-      mv -v $CLI_BINARY_PATH.exe ./$OUTPUT_DIR/$PROJECT_BINDING_NAME-$BINDING_ABI.exe
+      mv -v $CLI_BINARY_PATH.exe $OUTPUT_DIR/$PROJECT_BINDING_NAME-$BINDING_ABI.exe
   fi
 
   echo "-------------------输出-------------------"
   echo "移动前：$CLI_BINARY_PATH"
-  echo "移动后：./$OUTPUT_DIR/$PROJECT_BINDING_NAME-$BINDING_ABI"
+  echo "移动后：$OUTPUT_DIR/$PROJECT_BINDING_NAME-$BINDING_ABI"
   echo "-------------------输出-------------------"
 done

3b835c9d8f784e6f42ff678a2d10a601307ace52
MiniOcean404 <1037306928@qq.com>
2024-08-11 21:55:05 +0800

    👷 construction_worker: 测试

--- bindings/binding_compress_wasm/src/compress/start.rs
+++ bindings/binding_compress_wasm/src/compress/start.rs
@@ -1,5 +1,5 @@
 // use image_compress::compress::index::ImageCompression;
-use wasm_bindgen::prelude::*;
+// use wasm_bindgen::prelude::*;
 
 // pub async fn hello_world(file: String, quality: i8) -> Result<(), JsValue> {
 //     let mut info = ImageCompression::new(file, quality);

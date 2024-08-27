d50ad75e8f61e95d716e0f978fddd27ab595566b
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-08-27 15:03:01 +0800

    📝 memo: 移除警告

--- crates/utils/src/path/ext.rs
+++ crates/utils/src/path/ext.rs
@@ -1,6 +1,6 @@
 use std::{ffi::OsStr, path::Path};
 
-pub fn change_file_ext<'a>(path: &str, ext: &str) -> String {
+pub fn change_file_ext(path: &str, ext: &str) -> String {
     let path = Path::new(path);
     // let filename_no_ext = path.file_stem().unwrap_or(OsStr::new(""));
     path.with_extension(ext).to_string_lossy().to_string()
